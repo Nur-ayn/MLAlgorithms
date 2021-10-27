d3683a414e4a395ab5afc53f4bd80ad51b738ed7
Nur-ayn <nuraynhashim@outlook.com>
2021-10-27 20:25:21 +0200

    adding plot to display performance of tree

--- decision_tree.py
+++ decision_tree.py
@@ -14,23 +14,9 @@ class DataPoint:
     label: float
 
 
-dataset = [
-           [[1], 1],
-           [[2], 2],
-           [[3], 3],
-           [[4], 4],
-           [[0], 0],
-           [[-1], -1],
-           [[-2], -2],
-           [[-3], -3],
-           [[-4], -4]
-           ]
+dataset = [ [[x], 0.2*x**2 + x  + 0.2*x**3] for x in range(-15, 15) ]
 
-dataset = [ [[x], x] for x in range(-15, 15) ]
-
-datapoints = [ DataPoint(point[0], point[1]) for point in dataset ]
-datapoints = np.array(datapoints)
-
+datapoints = np.array([ DataPoint(point[0], point[1]) for point in dataset ])
 
 x = [ point.input[0] for point in datapoints ]
 y = [ point.label for point in datapoints ]
@@ -49,9 +35,8 @@ class Node:
 
     def next_node(self, test_point):
         if self.split_value is None:
-            return f"the mean is {self.mean}"
-        print('split value', self.split_value)
-        print('left and right', self.left, self.right)
+            return self.mean
+
         if test_point[self.dimension] < self.split_value:
             return self.left.next_node(test_point)
         if test_point[self.dimension] >= self.split_value:
@@ -62,35 +47,26 @@ class Node:
             minimal square loss -- needs to be optimised with some analysis'''
         splits = []
         for dimension in range(len(self.data[0].input)):
-            #print('DATADATADATa', self.data)
-            values = [ datapoint.input[dimension] for datapoint in self.data]
-            #print("ALL THE ValUEs", values)
+            values = [ datapoint.input[dimension] for datapoint in self.data] # optimise double for
             for value in values:
                 split = [dimension, value]
                 left, right = split_data(self.data, np.array([ datapoint.input[dimension] for datapoint in self.data ]) < value)
-                # print('left points', left)
-                # print('right_points', right)
-                total_loss = self.loss(left, split) + self.loss(right, split)
+            
+                total_loss = self.loss(left) + self.loss(right)
                 split.append(total_loss)
                 splits.append(split)
 
         splits = np.array(splits)
-        #print('considered splits', splits)
-        
-        #print('mean', self.mean)
         optimal_split = splits[np.argmin(splits[:,2])]  #getting split with smallest loss
-        # print('optimal split', optimal_split)
 
-        return optimal_split[0], optimal_split[1]
+        return int(optimal_split[0]), optimal_split[1]
 
     def split_node(self, levels=6):
         if levels == 0:
-            self.plot_node()
-            print("done training")
+            print("reached max depth")
             return
         dimension, value = self.best_split()
-        print('best split', dimension, value)
-        self.dimension = int(dimension)
+        self.dimension = dimension
 
         left, right = split_data(self.data, np.array([ datapoint.input[self.dimension] for datapoint in self.data ]) < value)
 
@@ -100,32 +76,19 @@ class Node:
 
         self.split_value = value
 
-        if len(left) != 0:
-            self.left = Node(left)
-            self.left.split_node(levels-1)
-            
-        if len(right) != 0:
-            self.right = Node(right)
-            self.right.split_node(levels-1)
+        self.left = Node(left)
+        self.right = Node(right)
+        self.left.split_node(levels-1)
+        self.right.split_node(levels-1)
 
     @staticmethod
-    def loss(datapoints, split):
+    def loss(datapoints):
         if len(datapoints) == 0:
             return 0
             
         mean_label = np.mean( [datapoint.label for datapoint in datapoints])
-        print([ (datapoint.label - mean_label)**2 for datapoint in datapoints ])
+
         return 1/len(datapoints)*np.sum([ (datapoint.label - mean_label)**2 for datapoint in datapoints ])
-
-    def plot_node(self):
-        datapoints = self.data
-        x = np.array([ datapoint.input[0] for datapoint in datapoints ])
-        y = [self.mean for _ in range(len(x)) ]
-        print("TRYING TO PLOT ", x, y)
-        plt.scatter(x, y, zorder=5)
-        plt.draw()
-
-
 
 
 class Tree:
@@ -141,18 +104,15 @@ class Tree:
         return [ self.root.next_node(input) for input in inputs ]
 
 
-print("--------------------------START------------------------------------------")
+if __name__ == '__main__':
+    print("--------------------------START------------------------------------------")
 
-root_node = Node(datapoints)
-decision_tree = Tree(root_node, depth=10)
-decision_tree.train()
-inputs = [[0],
-          [-10],
-          [3.5],
-          [1.2],
-          [10.5]
-         ]
-print(decision_tree.predict(inputs))
+    root_node = Node(datapoints)
+    decision_tree = Tree(root_node, depth=10)
+    decision_tree.train()
+    inputs = [[pt] for pt in np.linspace(-15, 15, 200)]
 
-
-plt.show()
+    outputs = decision_tree.predict(inputs)
+    print(outputs)
+    plt.plot(inputs, outputs, 'k')
+    plt.show()
